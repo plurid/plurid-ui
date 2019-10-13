1fc825e85dd95826415a5d99a913d401e9a78cb6
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-10-13 11:10:06 +0300

    setup: initial commit

---
+++ packages/react/rollup.config.js
@@ -0,0 +1,33 @@
+import typescript from 'rollup-plugin-typescript2';
+
+import pkg from './package.json';
+
+
+
+export default {
+    input: 'source/index.ts',
+    output: [
+        {
+            file: pkg.main,
+            format: 'cjs',
+            exports: 'named',
+            sourcemap: false,
+        },
+        {
+            file: pkg.module,
+            format: 'es',
+            exports: 'named',
+            sourcemap: false,
+        },
+    ],
+    external: [
+        'styled-components',
+    ],
+    plugins: [
+        typescript({
+            check: false,
+            rollupCommonJSResolveHack: true,
+            clean: true,
+        }),
+    ],
+}
---
+++ packages/react/source/index.ts
@@ -0,0 +1,16 @@
+import {
+    StyledButton,
+    StyledLinkButton,
+} from './components/buttons';
+
+import {
+    StyledSwitch,
+} from './components/inputs';
+
+
+
+export default {
+    Button: StyledButton,
+    LinkButton: StyledLinkButton,
+    Switch: StyledSwitch,
+}
