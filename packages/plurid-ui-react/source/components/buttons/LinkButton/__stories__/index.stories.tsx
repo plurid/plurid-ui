b5dc7f06bbf6145edf72903b818d35dd289376b3
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-10-16 13:37:38 +0300

    plurid-ui-react > dev: LinkButton

--- packages/plurid-ui-react/source/components/buttons/LinkButton/__stories__/index.stories.tsx
+++ packages/plurid-ui-react/source/components/buttons/LinkButton/__stories__/index.stories.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import themes from '@plurid/plurid-themes';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import {
+    withKnobs,
+    boolean,
+    number,
+} from '@storybook/addon-knobs';
 
 import LinkButton from '../';
 
@@ -14,10 +19,18 @@ const actions = {
 storiesOf(
     'LinkButton',
     module,
-).add('with text', () => {
+)
+.addDecorator(withKnobs)
+.add('with text', () => {
+    const inline = boolean('Inline', false);
+    const level = number('Level', 0);
+
     return (
         <LinkButton
             theme={themes.light}
+            inline={inline}
+            level={level}
+
             text="Link Button"
             {...actions}
         />
