6cd0d76960fbd6457927982198b46701438c7361
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-10-24 17:45:26 +0300

    plurid-ui-react > dev: Textline

---
+++ packages/plurid-ui-react/source/components/inputs/Textline/styled.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+
+
+export const StyledTextline: any = styled.label`
+    position: relative;
+    display: inline-block;
+    width: 60px;
+    height: 34px;
+
+    input {
+        opacity: 0;
+        width: 0;
+        height: 0;
+    }
+`;
