1a76c35e79d6b9ab0469f4bc0ee1b3b33f4102e9
Jan Cavel <caveljan@gmail.com>
2019-10-30 05:57:45 +0200

    plurid-ui-react > dev: Spinner, storybook

--- packages/plurid-ui-react/source/components/markers/Spinner/__stories__/index.stories.tsx
+++ packages/plurid-ui-react/source/components/markers/Spinner/__stories__/index.stories.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import themes from '@plurid/plurid-themes';
+
 import { storiesOf } from '@storybook/react';
-import { withKnobs, select } from '@storybook/addon-knobs';
+import {
+    withKnobs,
+    select,
+} from '@storybook/addon-knobs';
 
 import PluridSpinner from '../';
+
+import {
+    themeLabel,
+    themeOptions,
+    defaultThemeValue,
+} from '../../../../utilities/storybook';
 
 
 
@@ -20,7 +31,9 @@ storiesOf(
     module,
 )
 .addDecorator(withKnobs)
-.add('simple paragraph', () => {
+.add('basic', () => {
+    const theme = select(themeLabel, themeOptions, defaultThemeValue);
+
     const size = select(label, options, defaultValue);
     const _size = size === 'small'
         || size === 'normal'
@@ -30,6 +43,7 @@ storiesOf(
 
     return (
         <PluridSpinner
+            theme={themes[theme]}
             size={_size}
         />
     );
