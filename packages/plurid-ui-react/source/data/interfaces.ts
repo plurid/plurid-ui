c2c4072a6d64763c12f4244ce720224019ad8d41
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-01-17 14:49:02 +0200

    plurid-ui-react > dev: ProgressCircle, sizes

--- packages/plurid-ui-react/source/data/interfaces.ts
+++ packages/plurid-ui-react/source/data/interfaces.ts
@@ -2,3 +2,10 @@ export interface PluridDropdownSelectable {
     id: string;
     value: string;
 }
+
+
+export type Small = 'small';
+export type Normal = 'normal';
+export type Large = 'large';
+
+export type Sizes = Small | Normal | Large;
