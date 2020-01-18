f37a2b6327a4a0a846df460352224c45c57c0829
Jan Cavel <caveljan@gmail.com>
2020-01-18 11:13:37 +0200

    plurid-ui-react > dev: Tooltip

---
+++ packages/plurid-ui-react/source/components/markers/Tooltip/__stories__/index.stories.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import themes from '@plurid/plurid-themes';
+
+import { storiesOf } from '@storybook/react';
+import {
+    withKnobs,
+    select,
+    boolean,
+} from '@storybook/addon-knobs';
+
+import PluridTooltip from '../';
+
+import {
+    themeLabel,
+    themeOptions,
+    defaultThemeValue,
+} from '../../../../utilities/storybook';
+
+
+
+const label = 'Size';
+const options = {
+    small: 'small',
+    normal: 'normal',
+    large: 'large',
+    none: null,
+};
+const defaultValue = 'normal';
+
+storiesOf(
+    'PluridTooltip',
+    module,
+)
+.addDecorator(withKnobs)
+.add('basic', () => {
+    const theme = select(themeLabel, themeOptions, defaultThemeValue);
+
+    const size = select(label, options, defaultValue);
+    const _size = size === 'small'
+        || size === 'normal'
+        || size === 'large'
+            ? size
+            : 'normal';
+
+    const indicator = boolean('Indicator', true);
+
+    return (
+        <div
+            style={{
+                display: 'grid',
+                placeContent: 'center',
+            }}
+        >
+            <PluridTooltip
+                icon="?"
+                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                theme={themes[theme]}
+                size={_size}
+                indicator={indicator}
+            />
+        </div>
+    );
+});
---
+++ packages/plurid-ui-react/source/components/markers/Tooltip/styled.tsx
@@ -0,0 +1,99 @@
+import styled from 'styled-components';
+
+import {
+    fontFamilySansSerif,
+} from '../../../data/constants'
+
+import {
+    Theme,
+} from '@plurid/plurid-themes';
+
+
+
+interface IStyledTooltip {
+    theme: Theme;
+}
+
+export const StyledTooltip = styled.div<IStyledTooltip>`
+    position: relative;
+    display: grid;
+    place-content: center;
+    height: 20px;
+    width: 20px;
+    border-radius: 20px;
+    font-size: 12px;
+    line-height: 1.4;
+    text-align: left;
+
+    font-family: ${fontFamilySansSerif};
+    color: ${(props: IStyledTooltip) => {
+        return props.theme.colorPrimary;
+    }};
+    background-color: ${(props: IStyledTooltip) => {
+        return props.theme.backgroundColorSecondary;
+    }};
+`;
+
+
+export const StyledTooltipIcon = styled.div`
+    user-select: none;
+    cursor: pointer;
+`;
+
+
+interface IStyledTooltipText {
+    theme: Theme;
+    indicator: boolean;
+}
+
+export const StyledTooltipText = styled.div<IStyledTooltipText>`
+    position: absolute;
+    top: 30px;
+    left: 50%;
+    transform: translateX(-50%);
+    border-radius: 2px;
+    z-index: 999;
+    padding: 20px;
+    min-width: 150px;
+    font-size: 12px;
+    line-height: 1.3;
+    text-align: left;
+
+    color: ${(props: IStyledTooltip) => {
+        return props.theme.colorPrimary;
+    }};
+    background-color: ${(props: IStyledTooltip) => {
+        return props.theme.backgroundColorSecondary;
+    }};
+    box-shadow: ${(props: IStyledTooltip) => {
+        return props.theme.boxShadowUmbra;
+    }};
+
+    :before {
+        content: '';
+        position: absolute;
+        top: -10px;
+        left: 50%;
+        transform: translateX(-50%);
+        border-left: ${(props: IStyledTooltipText) => {
+            if (props.indicator) {
+                return '10px';
+            }
+            return '0';
+        }} solid transparent;
+        border-right: ${(props: IStyledTooltipText) => {
+            if (props.indicator) {
+                return '10px';
+            }
+            return '0';
+        }} solid transparent;
+        border-bottom: ${(props: IStyledTooltipText) => {
+            if (props.indicator) {
+                return '10px';
+            }
+            return '0';
+        }} solid ${(props: IStyledTooltip) => {
+            return props.theme.backgroundColorSecondary;
+        }};
+    }
+`;
