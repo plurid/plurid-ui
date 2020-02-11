3e013c65d504cd437975327d48aa6c9b8681927a
Jan Cavel <caveljan@gmail.com>
2020-02-11 21:41:11 +0200

    plurid-ui-react > dev: Formbutton, FormObliterate

--- packages/plurid-ui-react/source/components/form/FormObliterate/styled.tsx
+++ packages/plurid-ui-react/source/components/form/FormObliterate/styled.tsx
@@ -4,6 +4,10 @@ import {
     Theme,
 } from '@plurid/plurid-themes';
 
+import {
+    fontFamilySansSerif,
+} from '../../../data/constants';
+
 
 
 interface IStyledFormObliterate {
@@ -13,4 +17,21 @@ interface IStyledFormObliterate {
 }
 
 export const StyledFormObliterate = styled.div<IStyledFormObliterate>`
+    color: ${(properties: IStyledFormObliterate) => {
+        return properties.theme.colorPrimary;
+    }};
+    font-family: ${fontFamilySansSerif};
+    font-size: 0.8rem;
+    user-select: none;
+`;
+
+
+export const StyledObliterateContainer = styled.div`
+    text-align: center;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 30px 45px 60px;
+    align-items: center;
+    max-width: 200px;
+    margin: 1rem auto;
 `;
