b788a76ab49fd1ea4d7740122c58c4d8c5386b74
Jan Cavel <caveljan@gmail.com>
2020-02-11 21:15:54 +0200

    plurid-ui-react > dev: FormObliterate

---
+++ packages/plurid-ui-react/source/components/form/FormObliterate/styled.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+import {
+    Theme,
+} from '@plurid/plurid-themes';
+
+
+
+interface IStyledFormObliterate {
+    theme: Theme,
+    level: number;
+    devisible: boolean;
+}
+
+export const StyledFormObliterate = styled.div<IStyledFormObliterate>`
+
+`;
