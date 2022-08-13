4939885b8bf0ad1dfe3a6c2830d857d57385bdbd
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-08-13 11:47:01 +0300

    plurid-ui-components-react > setup: cleanup storybook

---
+++ packages/plurid-ui-components-react/unsource/components/pluridal/sitting/SittingTray/components/Sittings/index.tsx
@@ -0,0 +1,113 @@
+// #region imports
+    // #region libraries
+    import React from 'react';
+
+    import { AnyAction } from 'redux';
+    import { connect } from 'react-redux';
+    import { ThunkDispatch } from 'redux-thunk';
+
+    import {
+        Theme,
+    } from '@plurid/plurid-themes';
+
+    import {
+        PluridIconSittings,
+    } from '@plurid/plurid-icons-react';
+    // #endregion libraries
+
+
+    // #region external
+    import {
+        StyledSittingTrayButton,
+        StyledSittingTrayButtonIcon,
+        StyledSittingTrayButtonText,
+    } from '../../styled';
+    // #endregion external
+
+
+    // #region internal
+    import {
+        StyledSittings,
+    } from './styled';
+    // #endregion internal
+// #endregion imports
+
+
+
+// #region module
+export interface SittingsOwnProperties {
+    selectors: any;
+}
+
+export interface SittingsStateProperties {
+    stateInteractionTheme: Theme;
+}
+
+export interface SittingsDispatchProperties {
+}
+
+export type SittingsProperties =
+    & SittingsOwnProperties
+    & SittingsStateProperties
+    & SittingsDispatchProperties;
+
+
+const Sittings: React.FC<SittingsProperties> = (
+    properties,
+) => {
+    // #region properties
+    const {
+        // #region state
+        stateInteractionTheme,
+        // #endregion state
+    } = properties;
+    // #endregion properties
+
+
+    // #region render
+    return (
+        <StyledSittings
+            theme={stateInteractionTheme}
+        >
+            <StyledSittingTrayButton>
+                <StyledSittingTrayButtonIcon>
+                    <PluridIconSittings
+                        theme={stateInteractionTheme}
+                    />
+                </StyledSittingTrayButtonIcon>
+
+                <StyledSittingTrayButtonText>
+                    sittings
+                </StyledSittingTrayButtonText>
+            </StyledSittingTrayButton>
+        </StyledSittings>
+    );
+    // #endregion render
+}
+
+
+const mapStateToProperties = (
+    state: any,
+    ownProperties: any,
+): SittingsStateProperties => ({
+    stateInteractionTheme: ownProperties.selectors.themes.getInteractionTheme(state),
+});
+
+
+const mapDispatchToProperties = (
+    dispatch: ThunkDispatch<{}, {}, AnyAction>,
+): SittingsDispatchProperties => ({
+});
+
+
+const ConnectedSittings = connect(
+    mapStateToProperties,
+    mapDispatchToProperties,
+)(Sittings);
+// #endregion module
+
+
+
+// #region exports
+export default ConnectedSittings;
+// #endregion exports
